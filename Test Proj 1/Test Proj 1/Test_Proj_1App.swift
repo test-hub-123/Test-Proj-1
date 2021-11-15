7107fe356795f830837eadf2c996c94d99cffbce
test-hub-123 <94389614+test-hub-123@users.noreply.github.com>
2021-11-15 22:19:46 +0530

    Initial Upload

---
+++ Test Proj 1/Test Proj 1/Test_Proj_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Proj_1App.swift
+//  Test Proj 1
+//
+//  Created by Anandhu on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_Proj_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
